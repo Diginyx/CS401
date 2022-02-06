0eb8e074f1863d082e0c8fb14fef62299ad7b402
Diginyx <josuextorres@gmail.com>
2022-02-05 20:50:06 -0700

    Initial commit

---
+++ info.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <?php
+        for($i = 1; $i <= 10; $i++) {
+            echo "<p> I'm number {$i}!!! </p>";
+        }
+    ?>
+</body>
+</html>
+
