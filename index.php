5a9ffde2f2abadce93346857e9d183bb214c4845
Diginyx <josuextorres@gmail.com>
2022-02-05 22:00:09 -0700

    Homepage HTML Done CSS Work in Progress

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-<?php include_once("index.html"); ?>
+<html>
+    <?php include_once("header.html"); ?>
+    <?php include_once("ForumTopicHeader.html") ; ?>
+    <?php include_once("TopicGenerator.html") ; ?>
+    <?php include_once("TopicList.html") ; ?>
+</html>
