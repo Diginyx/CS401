eece144a6facf0ebfb4e37a3b38e9a126b431d6c
josh <josuextorres@gmail.com>
2022-02-16 01:43:13 -0700

    Forum Main Page CSS done for now

--- ForumMainPage.php
+++ ForumMainPage.php
@@ -1,8 +1,14 @@
 <html>
     <!-- Forum Main Page -->
     <?php include_once("header.html"); ?>
-    <?php include_once("ForumMainPage/ForumTopicHeader.html") ; ?>
-    <?php include_once("ForumMainPage/TopicGenerator.html") ; ?>
-    <?php include_once("ForumMainPage/TopicList.html") ; ?>
+    <div class="float-parent-element">
+        <div id="ForumMain">
+            <?php include_once("ForumMainPage/ForumTopicHeader.html") ; ?>
+            <?php include_once("ForumMainPage/TopicGenerator.html") ; ?>
+        </div>
+        <div id="ForumTopicList">
+            <?php include_once("ForumMainPage/TopicList.html") ; ?>
+        </div>
+    </div>
     <?php include_once("footer.html") ; ?>
 </html>
