b2f578cd8d6a220996adc9cd9d67cc0cb337f2d9
Diginyx <josuextorres@gmail.com>
2022-03-26 20:44:07 -0600

    all has been fixed

--- BlogPage.php
+++ BlogPage.php
@@ -1,7 +1,16 @@
 <html>
-    <?php include_once("header.html"); ?>
+    <?php include_once("header.php"); ?>
     <?php include_once("BlogPage/BlogHeader.html"); ?>
     <?php include_once("BlogPage/BlogPost.html"); ?>
-    <?php include_once("BlogPage/CommentGenerator.html"); ?>
+    <?php
+        if (isset($_SESSION["access_granted"]) && $_SESSION["access_granted"])
+        {
+    ?>
+        <?php include_once("BlogPage/CommentGenerator.html"); ?>
+    <?php 
+        }else{
+            echo "<h3>" . 'Sign in to create topic' . "</h3>";
+        }
+    ?>
     <?php include_once("footer.html"); ?>
 </html>
